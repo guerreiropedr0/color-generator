a040efb47f6eeb0256666d50415e7ef3abae05c3
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-02-05 01:46:50 +0000

    Update README.md and MIT.md files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/script.js',
   devServer: {
     static: './dist',
